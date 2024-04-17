79c407d9df6f0c259f321766b655eedd2064e3a4
Nicolae Pogila <nikupogila@gmail.com>
2024-04-17 14:48:51 +0300

    Make the Landing page

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'admin');
 define('DB_NAME', 'crud');
- 
+
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-if($mysqli === false){
+
+if ($mysqli->connect_error) {
     die("ERROR: Could not connect. " . $mysqli->connect_error);
 }
 ?>
